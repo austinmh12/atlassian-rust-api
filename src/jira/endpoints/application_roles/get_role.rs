a9a29a454d428c0196b946333c969e6bec9f98d6
Austin Heil <austinmh12@gmail.com>
2026-02-18 14:28:47 -0500

    GET applicationrole/:key

---
+++ src/jira/endpoints/application_roles/get_role.rs
@@ -0,0 +1,45 @@
+use std::sync::Arc;
+
+use crate::{Jira, Result, rest_client::RestClient, web::Endpoint};
+
+#[derive(Debug, Clone)]
+pub struct GetRoleBuilder {
+	client: Arc<RestClient>,
+	request: GetRoleRequest,
+}
+
+#[derive(Debug, Clone, Default)]
+struct GetRoleRequest {
+	key: String,
+}
+
+impl Endpoint for GetRoleRequest {
+	fn endpoint(&self) -> std::borrow::Cow<'static, str> {
+		format!("applicationrole/{}", &self.key).into()
+	}
+}
+
+impl GetRoleBuilder {
+	fn new(client: Arc<RestClient>) -> GetRoleBuilder {
+		GetRoleBuilder { client, request: GetRoleRequest::default() }
+	}
+
+	fn key(mut self, key: impl Into<String>) -> GetRoleBuilder {
+		self.request.key = key.into();
+		self
+	}
+
+	async fn send(self) -> Result<serde_json::Value> {
+		self.client.get(self.request).await
+	}
+}
+
+crate::macros::futurize!(GetRoleBuilder, serde_json::Value);
+
+impl Jira {
+	/// Returns the ApplicationRole with the given key if it exists. Returns a 404 if the ApplicationRole
+	/// is not found.
+	pub async fn get_role(&self, key: impl Into<String>) -> Result<serde_json::Value> {
+		GetRoleBuilder::new(Arc::clone(&self.client)).key(key).await
+	}
+}
