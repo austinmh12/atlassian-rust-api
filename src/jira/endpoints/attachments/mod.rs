51e45ed0fb99beaf5b3742a90d1b53a76f2e836c
Austin Heil <austinmh12@gmail.com>
2026-02-18 15:03:24 -0500

    Attachment endpoints

---
+++ src/jira/endpoints/attachments/mod.rs
@@ -0,0 +1,8 @@
+mod get_attachment;
+mod remove_attachment;
+mod get_attachment_meta;
+
+// #[cfg(feature = "experimental")]
+mod expand_attachment_for_humans;
+// #[cfg(feature = "experimental")]
+mod expand_attachment_for_robots;
--- src/jira/endpoints/mod.rs
+++ src/jira/endpoints/mod.rs
@@ -1,6 +1,7 @@
 // Modules
 mod application_properties;
 mod application_roles;
+mod attachments;
 mod fields;
 mod issues;
 mod permissions;
