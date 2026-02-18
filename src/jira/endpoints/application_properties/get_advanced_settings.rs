e04ad1f8ea710beded1643aaec5a067c71f792b4
Austin Heil <austinmh12@gmail.com>
2026-02-18 10:07:28 -0500

    GET application-properties/advanced-settings

---
+++ src/jira/endpoints/application_properties/get_advanced_settings.rs
@@ -0,0 +1,37 @@
+use std::sync::Arc;
+
+use crate::{Jira, Result, rest_client::RestClient, web::Endpoint};
+
+#[derive(Debug, Clone)]
+pub struct GetAdvancedSettingsBuilder {
+	client: Arc<RestClient>,
+	request: GetAdvancedSettingsRequest,
+}
+
+#[derive(Debug, Clone, Default)]
+struct GetAdvancedSettingsRequest;
+
+impl Endpoint for GetAdvancedSettingsRequest {
+	fn endpoint(&self) -> std::borrow::Cow<'static, str> {
+		"application-properties/advanced-settings".into()
+	}
+}
+
+impl GetAdvancedSettingsBuilder {
+	pub(crate) fn new(client: Arc<RestClient>) -> GetAdvancedSettingsBuilder {
+		GetAdvancedSettingsBuilder { client, request: GetAdvancedSettingsRequest::default() }
+	}
+
+	async fn send(self) -> Result<serde_json::Value> {
+		self.client.get(self.request).await
+	}
+}
+
+crate::macros::futurize!(GetAdvancedSettingsBuilder, serde_json::Value);
+
+impl Jira {
+	/// Returns the properties that are displayed on the "General Configuration > Advanced Settings" page.
+	pub async fn get_advanced_settings(&self) -> Result<serde_json::Value> {
+		GetAdvancedSettingsBuilder::new(Arc::clone(&self.client)).await
+	}
+}
