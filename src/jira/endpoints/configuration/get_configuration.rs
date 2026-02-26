8b82c5bca28cf363c97e9547ee95d6fafdfa9803
Austin Heil <austin.heil@saabinc.com>
2026-02-26 09:51:03 -0500

    Get configuration endpoint

---
+++ src/jira/endpoints/configuration/get_configuration.rs
@@ -0,0 +1,34 @@
+use std::sync::Arc;
+
+use crate::{Jira, Result, rest_client::RestClient, web::Endpoint};
+
+#[derive(Debug, Clone)]
+pub struct GetConfigurationBuilder {
+	client: Arc<RestClient>,
+	request: GetConfigurationRequest,
+}
+
+#[derive(Debug, Clone, Default)]
+struct GetConfigurationRequest;
+
+impl Endpoint for GetConfigurationRequest {
+	fn endpoint(&self) -> std::borrow::Cow<'static, str> {
+		"configuration".into()
+	}
+}
+
+impl GetConfigurationBuilder {
+	fn new(client: Arc<RestClient>) -> GetConfigurationBuilder {
+		GetConfigurationBuilder { client, request: GetConfigurationRequest::default() }
+	}
+
+	pub async fn send(self) -> Result<serde_json::Value> {
+		self.client.get(self.request).await
+	}
+}
+
+impl Jira {
+	pub fn get_configuration(&self) -> GetConfigurationBuilder {
+		GetConfigurationBuilder::new(Arc::clone(&self.client))
+	}
+}
