e2abf8e37d47af69a7d25c0f4f1f128895adf5ff
Austin Heil <austin.heil@saabinc.com>
2026-02-27 08:37:51 -0500

    Delete filter endpoint

---
+++ src/jira/endpoints/filters/delete_filter.rs
@@ -0,0 +1,42 @@
+use std::sync::Arc;
+
+use crate::{Jira, Result, rest_client::RestClient, web::Endpoint};
+
+#[derive(Debug, Clone)]
+pub struct DeleteFilterBuilder {
+	client: Arc<RestClient>,
+	request: DeleteFilterRequest,
+}
+
+#[derive(Debug, Clone, Default)]
+struct DeleteFilterRequest {
+	id: u64,
+}
+
+impl Endpoint for DeleteFilterRequest {
+	fn endpoint(&self) -> std::borrow::Cow<'static, str> {
+		format!("filter/{}", self.id).into()
+	}
+}
+
+impl DeleteFilterBuilder {
+	fn new(client: Arc<RestClient>) -> DeleteFilterBuilder {
+		DeleteFilterBuilder { client, request: DeleteFilterRequest::default() }
+	}
+
+	fn id(mut self, id: u64) -> DeleteFilterBuilder {
+		self.request.id = id;
+		self
+	}
+
+	pub async fn send(self) -> Result<()> {
+		self.client.get(self.request).await
+	}
+}
+
+impl Jira {
+	/// Delete a filter.
+	pub fn delete_filter(&self, id: u64) -> DeleteFilterBuilder {
+		DeleteFilterBuilder::new(Arc::clone(&self.client)).id(id)
+	}
+}
